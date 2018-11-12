1839abd9d63aab08b13ffcedf633d254b25037f2
Vivian Chaizemartin <vivian.chaizemartin@epita.fr>
2018-11-12 11:42:17 +0100

    [APP][UTILS] Add command and close window handlers

---
+++ chaize_vUdokotela/Udokotela/RelayCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace Udokotela
+{
+    public class RelayCommand : ICommand
+    {
+        #region variables
+        /// <summary>
+        /// action à executer
+        /// </summary>
+        readonly Action<object> _execute;
+        /// <summary>
+        /// test pour savoir si on peut executer la commande
+        /// </summary>
+        readonly Predicate<object> _canExecute;
+        #endregion 
+
+
+        #region Constructeur
+        /// <summary>
+        /// Crée une nouvelle commande que l'on peut toujours executer
+        /// </summary>
+        /// <param name="execute">Le code à executer</param>
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {}
+
+        /// <summary>
+        /// Crée une nouvelle commande
+        /// </summary>
+        /// <param name="execute">le code à executer</param>
+        /// <param name="canExecute">Si on peut executer ou pas</param>
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+        #endregion
+
+
+        #region ICommand Members
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+        #endregion // ICommand Members
+    }
+}
---
+++ chaize_vUdokotela/Udokotela/WindowCloseBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace Udokotela
+{
+    /// <summary>
+    /// permet de fermer une fenêtre par binding MVVM
+    /// </summary>
+    public static class WindowCloseBehaviour
+    {
+        public static readonly DependencyProperty CloseProperty = DependencyProperty.RegisterAttached("Close", typeof(bool), typeof(WindowCloseBehaviour), new UIPropertyMetadata(false, OnClose));
+
+        public static void SetClose(DependencyObject target, bool value)
+        {
+            target.SetValue(CloseProperty, value);
+        }
+
+        private static void OnClose(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is bool && ((bool)e.NewValue))
+            {
+                Window window = GetWindow(sender);
+                if (window != null)
+                    window.Close();
+            }
+        }
+
+        private static Window GetWindow(DependencyObject sender)
+        {
+            Window window = null;
+
+            if (sender is Window)
+                window = (Window)sender;
+
+            if (window == null)
+                window = Window.GetWindow(sender);
+
+            return window;
+        }
+    }
+}
