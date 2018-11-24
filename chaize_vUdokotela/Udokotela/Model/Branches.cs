ae8bc0bd7d7d42028e32cb1949abb9deb5e54045
vivian chaizemartin <vivian.chaizemartin@epita.fr>
2018-11-24 16:59:07 +0100

    [MAIN][XAML] First implementation for TreeView in left side bar

---
+++ chaize_vUdokotela/Udokotela/Model/Branches.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Udokotela.Model
+{
+    public class Branches
+    {
+        public string Name { get; set; }
+        public ObservableCollection<Branches> SubBranches { get; set; }
+
+        public Branches()
+        {
+            SubBranches = new ObservableCollection<Branches>();
+        }
+
+        public Branches(string name, ICollection<Branches> collection)
+        {
+            Name = name;
+            SubBranches = new ObservableCollection<Branches>(collection);
+        }
+    }
+}
