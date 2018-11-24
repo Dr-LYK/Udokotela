321f944bd7cce5f0ce5b402f382de10ae10b1b37
SeiyaR <1magnum78630@gmail.com>
2018-11-24 20:26:32 +0100

    [OBSERVATION] adding observation connected service

---
+++ chaize_vUdokotela/Udokotela/Services/CSObservation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Udokotela.ServiceObservation;
+
+namespace Udokotela.Services
+{
+    public class CSObservation: ConnectedService<IServiceObservation>
+    {
+        private IServiceObservation _client;
+
+        // object MUST be ClientBase<IServiceObservation>, IServiceObservation
+        private CSObservation(object service)
+            : base((ClientBase<IServiceObservation>)service)
+        {
+            this._client = (IServiceObservation)service;
+        }
+
+        public CSObservation(): this(new ServiceObservationClient())
+        {
+        }
+
+        public bool AddObservation(int idPatient, Observation observation)
+        {
+            return this.Execute(() => this._client.AddObservationAsync(idPatient, observation), false);
+        }
+    }
+}
