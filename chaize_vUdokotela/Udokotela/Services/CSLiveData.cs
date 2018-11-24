8818c336d0b3394ad91c97e8fd89e4ad48c909df
SeiyaR <1magnum78630@gmail.com>
2018-11-24 20:35:48 +0100

    [LIVE DATA] Adding live data connected service

---
+++ chaize_vUdokotela/Udokotela/Services/CSLiveData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Udokotela.ServiceLive;
+
+namespace Udokotela.Services
+{
+    public class CSLiveData: ConnectedService<IServiceLive>
+    {
+        private IServiceLive _client;
+
+        // object MUST be ClientBase<IServiceLive>, IServiceLive
+        private CSLiveData(object service)
+            : base((ClientBase<IServiceLive>)service)
+        {
+            this._client = (IServiceLive)service;
+        }
+
+        public CSLiveData(): this(new ServiceLiveClient(null))
+        {
+            Subscribe();
+        }
+
+        private void Subscribe()
+        {
+            this.Execute(() => this._client.SubscribeAsync());
+        }
+    }
+}
