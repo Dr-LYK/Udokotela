bd45f5fde0f00ceba1fc907ffcd475d952cbae9c
vivian chaizemartin <vivian.chaizemartin@epita.fr>
2018-12-01 23:17:24 +0100

    [ViewModel][PatientSheet] Init PatientSheetViewModel.cs

---
+++ chaize_vUdokotela/Udokotela/ViewModel/PatientSheetViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udokotela.ServicePatient;
+
+namespace Udokotela.ViewModel
+{
+    /// <summary>
+    /// Classe de ViewModel pour la fiche patient.
+    /// </summary>
+    public class PatientSheetViewModel : BaseViewModel
+    {
+        #region Attributes
+        private Patient _patientToDisplay;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Numéro identifiant du patient.
+        /// </summary>
+        public string Id
+        {
+            get { return _patientToDisplay.Id.ToString(); }
+        }
+
+        /// <summary>
+        /// Nom de famille du patient.
+        /// </summary>
+        public string Name
+        {
+            get { return _patientToDisplay.Name; }
+        }
+
+        /// <summary>
+        /// Prénom du patient.
+        /// </summary>
+        public string FirstName
+        {
+            get { return _patientToDisplay.Firstname; }
+        }
+
+        /// <summary>
+        /// Date de naissance du patient.
+        /// </summary>
+        public string Birthdate
+        {
+            get { /*TODO*/ return null; }
+        }
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Methods
+        #endregion
+
+    }
+}
