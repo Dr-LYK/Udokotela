d7fa0e55492278b9b935a68d1bda420928c79337
SeiyaR <1magnum78630@gmail.com>
2018-12-01 12:57:56 +0100

    [ADDUSER] Can now add user

---
+++ chaize_vUdokotela/Udokotela/Utils/ImageLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Udokotela.Utils
+{
+    public class ImageLoader
+    {
+        public static byte[] Load(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                return null;
+            }
+            return LoadFileData(filename);
+        }
+
+        private static byte[] LoadFileData(string filename)
+        {
+            try
+            {
+                return File.ReadAllBytes(filename);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Error reading all bytes from {filename}");
+                return null;
+            }
+        }
+    }
+}
--- chaize_vUdokotela/Udokotela/ViewModel/AddUserViewModel.cs
+++ chaize_vUdokotela/Udokotela/ViewModel/AddUserViewModel.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Udokotela.Services;
+using Udokotela.ServiceUser;
+using Udokotela.Utils;
 
 namespace Udokotela.ViewModel
 {
@@ -176,7 +178,18 @@ namespace Udokotela.ViewModel
         /// </summary>
         private void Save()
         {
-            /* TODO */
+            byte[] imageData = ImageLoader.Load(this._picture);
+            User newUser = new User() { Connected = false, Firstname = this._firstName, Login = this._login, Name = this._name, Picture = imageData, Pwd = Converter.SecureStringToString(this._password), Role = this._role };
+            bool isUserAdded =_userService.AddUser(newUser);
+            if (isUserAdded)
+            {
+                this.CloseSignal = true;
+            }
+            else
+            {
+                // TODO
+                // Behavior in case save failed
+            }
         }
 
         /// <summary>
@@ -184,7 +197,7 @@ namespace Udokotela.ViewModel
         /// </summary>
         private void Cancel()
         {
-            /* TODO */ 
+            this.CloseSignal = true;
         }
 
         /// <summary>
@@ -192,7 +205,14 @@ namespace Udokotela.ViewModel
         /// </summary>
         private void OpenFile()
         {
-            /* TODO */
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "Image Files (*.jpeg;*.png;*.jpg;*.gif)|*.jpeg;*.png;*.jpg;*.gif";
+            bool? result = dlg.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                this.Picture = dlg.FileName;
+            }
         }
         #endregion
     }
